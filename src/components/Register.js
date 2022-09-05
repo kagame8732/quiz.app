722c987ef065057e5f552470d964592a26c7800c
kagame8732 <kagame8732@gmail.com>
2022-09-05 15:08:47 +0200

    another

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,24 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 
 const Register = () => {
+  const [visible, setVisible] = useState(false);
+  const handleToggle = () => {
+    setVisible(!visible);
+  };
+
   return (
-    <div className=" bg-gray-600 bg-opacity-70 w-96 h-80 px-4 py-4 rounded-md ml-auto mr-auto mt-32">
+    <div className=" bg-gray-600 bg-opacity-70 w-96 h-82 px-4 py-4 rounded-md ml-auto mr-auto mt-32">
       <form>
         <div className="">
           <h1 className="text-center font-bold text-2xl p-4 capitalize text-green-300 ">
             Welcome to Uzi
           </h1>
-          <div className="bg-white bg-opacity-40 p-2 rounded-md flex">
+          <div className="bg-white bg-opacity-40 p-2 rounded-md flex ">
             <input
               type="text"
               name="fullname"
-              placeholder="Enter FullName"
+              placeholder="Enter Full Name"
               required
               className="bg-white bg-opacity-30 rounded-sm p-2 w-full border-none placeholder-slate-900"
             />
           </div>
-          <div className="bg-white bg-opacity-40 p-2 rounded-md flex">
+          <div className="bg-white bg-opacity-40 p-2 rounded-md flex mt-3">
             <input
               type="email"
               name="email"
@@ -35,8 +41,24 @@ const Register = () => {
               className="bg-white bg-opacity-30 rounded-sm p-2 w-full border-0 placeholder-slate-900"
               required
             />
+            {visible === false ? (
+              <AiFillEyeInvisible
+                className="w-16 h-8 absolute top-4 right-2 cursor-pointer"
+                onClick={handleToggle}
+              />
+            ) : (
+              <AiFillEye
+                className="w-16 h-8 absolute top-4 right-2 cursor-pointer"
+                onClick={handleToggle}
+              />
+            )}
           </div>
           <div>
+            <div>
+              <button className="btn-text mt-2 text-green-400 cursor-pointer w-18 p-2 rounded-sm justify-center hover:bg-white hover:text-green-500 text-xl uppercase">
+                Register
+              </button>
+            </div>
             <div className="flex justify-center mt-4 ">
               <Link
                 to={"/quiz"}
