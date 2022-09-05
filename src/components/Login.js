45b1381516b94940325e16604f9b734434cb5793
kagame8732 <kagame8732@gmail.com>
2022-09-05 12:47:07 +0200

    another

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,6 +12,21 @@ function Login() {
             Welcome to Uzi
           </h1>
           <div className="bg-white bg-opacity-40 p-2 rounded-md flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-8 h-12"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
+
             <input
               type="email"
               name="email"
@@ -21,6 +36,21 @@ function Login() {
             />
           </div>
           <div className="bg-white bg-opacity-40 p-2 rounded-md flex mt-4 relative">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-8 h-12"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
+              />
+            </svg>
+
             <input
               type="password "
               name="password"
